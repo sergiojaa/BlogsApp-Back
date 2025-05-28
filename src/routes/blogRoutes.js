c2c3a74d7c37bf2c103fac7f8a2b740ca5da1503
seriojaa <kaliashvilisergi@gmail.com>
2025-05-28 12:04:32 +0400

    import update Blog to app

--- src/routes/blogRoutes.js
+++ src/routes/blogRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import {createBlog, getAllBlogs, getBlogById} from "../controller/blogController.js";
+import {createBlog, getAllBlogs, getBlogById, updateBlog} from "../controller/blogController.js";
 
 const router = express.Router();
+
 
 router.post('/create', createBlog);
 router.get('/',getAllBlogs);
 router.get('/:id', getBlogById);
+router.patch('/:id',updateBlog)
 
 export default router;
